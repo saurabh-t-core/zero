dc8e7737b763c836c0452d680d6476539ec2a43e
Saurabh Tripathi <saurabh.t.core@gmail.com>
2026-09-12 21:57:14 +0530

    final version

--- project1/main.js
+++ project1/main.js
@@ -1,4 +1,4 @@
 let q=document.getElementById("qt1")
 function quote(){
-    q.textContent="No man ever steps in the same river twice, for it's not the same river and he's not the same man."
+    q.textContent="No man ever steps in the same river twice, for it's not the same river and he's not the same man.       -Heraclitus"
 }
